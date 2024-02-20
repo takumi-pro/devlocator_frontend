0f6f4c53f527260f494ece625312a982c8038355
takumi <takumidiary.0927@gmail.com>
2024-02-20 20:51:14 +0900

    feature: 検索機能修正

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,7 @@
 import { format } from "date-fns";
 
+import { DAYS_TO_ADD_FOR_NEXT_DAY } from "@/const/date";
+
 /**
  * 指定された日付範囲をカンマ区切りの文字列に変換
  * @param startDate 開始日
@@ -16,7 +18,6 @@ export const dateRangeToString = (startDate: Date, endDate?: Date) => {
     dateStrings.push(formattedDate);
 
     // TODO: 別ファイルで管理
-    const DAYS_TO_ADD_FOR_NEXT_DAY = 1;
     currentDate.setDate(currentDate.getDate() + DAYS_TO_ADD_FOR_NEXT_DAY);
   }
 
